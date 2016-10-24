d906bc1b8990638fd861c676f209d562ffd6128a
apple <apple@apples-MacBook-Pro.local>
2016-10-24 17:22:12 +0530

    Added

---
+++ Examples/ViewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using UIKit;
+
+namespace Examples
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			this.Title = "Sample App";
+			this.EdgesForExtendedLayout = UIRectEdge.None;
+		}
+	}
+}
